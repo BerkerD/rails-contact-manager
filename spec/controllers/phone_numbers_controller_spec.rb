f351e3a1d164bda91cc8e41a3c8e57c8b5007925
berkerdemirer <demirerberker@gmail.com>
2018-11-20 01:26:27 +0300

    requirement for specifying user id while adding phone number is deleted.After adding new phone number, user is redirected to person details.  Validations done by using capybara.

--- spec/controllers/phone_numbers_controller_spec.rb
+++ spec/controllers/phone_numbers_controller_spec.rb
@@ -74,15 +74,18 @@ RSpec.describe PhoneNumbersController, type: :controller do
 
   describe "POST #create" do
     context "with valid params" do
+      let(:alice) { Person.create(first_name: 'Alice', last_name: 'Smith') }
+      let(:valid_attributes) { {number: '555-1234', person_id: alice.id} }
+
       it "creates a new PhoneNumber" do
         expect {
           post :create, params: {phone_number: valid_attributes}, session: valid_session
         }.to change(PhoneNumber, :count).by(1)
       end
 
-      it "redirects to the created phone_number" do
+      it "redirects to the phone number's person" do        
         post :create, params: {phone_number: valid_attributes}, session: valid_session
-        expect(response).to redirect_to(PhoneNumber.last)
+        expect(response).to redirect_to(alice)
       end
     end
 
