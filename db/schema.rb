256cadf8d314d0bf1bded94ffe0a48bedd11c106
berkerdemirer <demirerberker@gmail.com>
2018-11-20 03:33:47 +0300

    generated email_addresses and associated it with person

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_18_211957) do
+ActiveRecord::Schema.define(version: 2018_11_20_002605) do
+
+  create_table "email_addresses", force: :cascade do |t|
+    t.string "address"
+    t.integer "person_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "people", force: :cascade do |t|
     t.string "first_name"
