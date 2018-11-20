fd072d0494e30e60fbf2977a8fd712a485a0bab5
berkerdemirer <demirerberker@gmail.com>
2018-11-20 03:08:45 +0300

    delete phone number functionality added. finished implementing phone number functionality. all tests passed

--- spec/features/person_view_spec.rb
+++ spec/features/person_view_spec.rb
@@ -45,4 +45,16 @@ describe 'the person view', type: :feature do
     expect(page).to_not have_content(old_number)
   end
 
+  it 'has links to delete phone numbers' do
+    person.phone_numbers.each do |phone|
+      expect(page).to have_link('delete', href: phone_number_path(phone))
+    end
+  end
+
+  it 'deletes a phone number' do
+    phone = person.phone_numbers.first
+    first(:link, 'delete').click
+    expect(page).to_not have_content(phone.number)
+  end
+
 end
